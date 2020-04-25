7ef6504fb25229f2db63a675a5b0cf9d88b52e43
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-04-25 16:02:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(name='ezfuzz',
       version='1.0',
-      description='Ezfuzz is a Python package created to make fuzz testing an application easier.',
+      description='Ezfuzz is a Python package created to make fuzzing an application easier.',
       url='https://github.com/binexisHATT/Ezfuzz',
       author='Alexis Rodriguez',
       author_email='rodriguez10011999@gmail.com',
